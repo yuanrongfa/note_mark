5c0f7f4db613f1d841fcf48b0382cb8b2816e1f3
yuanrongfa <42547301+yuanrongfa@users.noreply.github.com>
2022-08-21 08:12:03 +0800

    增加求和sum()用法

---
+++ pandas笔记/统计救和.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import pandas as pd
+import numpy as np
+import os
+
+file =r'/Users/yuan/list/a1.xlsx'
+
+df = pd.read_excel(file,header=0)
+
+
+# 记录总条数
+i=df[df['类别']=='硬盘']['收入'].size
+print(i)
+
+
+# 收入统计总数
+df[df['类别']=='硬盘']['收入'].sum()
